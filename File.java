8cfd85789ae8e0e85a3d019009e39d5c7ad77a45
MaheshKumarGodela <94672409+204g1a0551@users.noreply.github.com>
2024-08-29 18:50:16 +0530

    Create File.java

---
+++ File.java
@@ -0,0 +1,27 @@
+package Alpha;
+public class File
+{
+    private String name;
+    private String content;
+    public File(String name, String content)
+    {
+        this.name=name;
+        this.content=content;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getContent()
+    {
+        return content;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public void setContent(String content)
+    {
+        this.content=content;
+    }
+}
